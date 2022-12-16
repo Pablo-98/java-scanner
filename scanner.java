81f883950081dcd09667f3f354d9ad58fa053dca
Pablo Hernandez <hpablo575@gmail.com>
2022-12-16 14:04:49 -0700

    first

---
+++ scanner.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class scanner{
+public static void main( String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    System.out.println("what is your name?");
+
+    System.out.println("what is your fav food");
+
+    System.out.println("what is your age?");
+
+    
+
+
+}
+}
