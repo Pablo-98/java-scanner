1d11343dfa7266ed956f34d9e84d6c6676657a59
Pablo Hernandez <hpablo575@gmail.com>
2022-12-16 14:14:56 -0700

    declared variables for each question and added user input with nextline and nextint.

--- scanner.java
+++ scanner.java
@@ -4,13 +4,31 @@ public class scanner{
 public static void main( String[] args) {
     Scanner input = new Scanner(System.in);
 
+    // three questions for the user
     System.out.println("what is your name?");
 
-    System.out.println("what is your fav food");
+    // variable declaration and initialization
+    // .nextLine(): provides a string
+    String name = input.nextLine();
+    
+
+    System.out.println("what is your fav food?");
+    //variable declaration
+    String food = input.nextLine();
 
     System.out.println("what is your age?");
+    //variable declaration
+    //.nextInt() provides an integer
+    int age = input.nextInt();
+    int year = 2022 - age;
 
-    
+    // we need a message that combines all the users input 
+    // concatenation: process of grouping 'gluing' strings with different data types toether
+
+    String response = "Hi, pleased to meet you " + name + "I also like " +food + "!!!" + " I bet you were born in " + year + "?, eh?";
+
+
+
 
 
 }
